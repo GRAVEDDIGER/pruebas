c4b3fcdedc9f28138818b6b1531ca1f5d6ba3cf1
Adrian Daniel Abadin <aabadin@gmail.com>
2022-05-14 11:21:54 -0300

    aparece con js

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-const adrian = nombre=>    document.write(`holas ${nombre}`);
-document.title="Chupala"
+const apareceArriba = idEtq => {
+const elemento =  document.getElementById(idEtq);
+// elemento.hidden=false;
+let clientrec = elemento.getBoundingClientRect()
+let pX =clientrec.left;
+let pY =clientrec.top;
+let he=clientrec.height;
 
-adrian("adruab")    
+pY=pY-he;
+elemento.style.top=pY+"px"
+elemento.style.opacity=1;
+
+setInterval(()=>{
+    if (pY<21){
+    console.log(pY)
+    pY++
+        elemento.style.top=pY+"px"}
+
+    else {clearInterval();}
+},2);
     
+    
+
+}
+
+
+
+
