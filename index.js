91ffeca4e39166f885c55d345affa578f3ad8c69
Adrian Daniel Abadin <aabadin@gmail.com>
2022-05-21 06:06:30 -0300

    Menu Terminado Responsive

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 function aparecer(idEtq,idMnu){
 let menu = document.getElementById(idEtq);
 let opcion =document.getElementById(idMnu);
-if (menu.classList.contains("mover")!=true){
-menu.classList.add("mover");
-opcion.classList.add("mnuSel");
-}else{menu.classList.remove("mover");
-opcion.classList.remove("mnuSel")
-}
+let hamburguesa = document.getElementById("menuPrincipal");
+menu.classList.toggle("mover")
+ opcion.classList.toggle("mnuSel")
+console.log("aparecio")
 }
 
 function hamb(id1,id2){
+
     let menu = document.getElementById(id1);
     let boton= document.getElementById(id2);
-    menu.classList.add("menuPrin");
+    let color1 =document.querySelector(".drop")
+    let estilo =window.getComputedStyle(color1,null)
+    let a=boton.clientWidth
+    console.log(a)
+console.log(estilo.getPropertyValue("color"));
+    if (a === 56){
+        console.log("why")
+        if(estilo.getPropertyValue("color") =="rgb(255, 255, 255)")
+       { menu.classList.toggle("menuPrin");}
+
+}
 }
